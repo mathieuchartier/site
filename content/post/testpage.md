9a0c3982a3f19f9e0f28ef7b89e66c4d09f27e02
Mathieu Chartier <mathieu.a.chartier@gmail.com>
2022-01-30 23:28:04 -0500

    Site update Sun Jan 30 23:28:02 EST 2022

---
+++ content/post/testpage.md
@@ -0,0 +1,6 @@
+---
+title: "Testpage"
+date: 2022-01-30T21:16:10-05:00
+draft: true
+---
+
---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+printf "Deploying..."
+msg="Site update $(date)"
+
+echo ""
+echo "Committing internal changes"
+cd public
+git add .
+git commit -m "$msg"
+git push origin master
+
+cd ..
+echo ""
+echo "Committing public changes"
+git add .
+git commit -m "$msg"
+git push origin master
